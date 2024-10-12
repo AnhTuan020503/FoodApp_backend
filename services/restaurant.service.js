172abff117155165b5cbda66724fb93517d4d0e8
AnhTuan020503 <151918220+AnhTuan020503@users.noreply.github.com>
2024-10-12 15:39:06 +0700

    new push

--- services/restaurant.service.js
+++ services/restaurant.service.js
@@ -27,5 +27,44 @@ const getAllRestaurant = async () =>{
             error :`Restaurant finding failed : ${error?.message}`,
         }
     }
+};
+const getOneRestaurantById = async (restaurantId) => {
+    try {
+        let restaurant = await MongoDB.db
+        .collection(mongoConfig.collections.RESTAURANTS)
+        .aggregate([
+            {
+              $match: {
+                id: restaurantId,
+              }
+            }, {
+              $lookup: {
+                from: 'foods', 
+                localField: 'id', 
+                foreignField: 'restaurantId', 
+                as: 'foods',
+              }
+            }
+          ])
+        .toArray()
+        if(restaurant && restaurant?.length > 0){
+            return {
+                status: true,
+                message: "Restaurant found successfully",
+                data: restaurant[0],
+            }
+        }else{
+            return {
+                status :false,
+                message: "No restaurant found",
+            }
+        }
+    } catch (error) {
+        return {
+            status :false,
+            message: "Restaurant finding failed",
+            error :`Restaurant finding failed : ${error?.message}`,
+        }
+    }
 }
-module.exports ={getAllRestaurant};
+module.exports ={getAllRestaurant,getOneRestaurantById};
